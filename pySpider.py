3785561268f890c047f1b7b5407cbac276fb3e0b
Gautam <gautham5678@gmail.com>
2012-04-19 23:07:19 +0530

    Working prototype 2, Using a List instead of a recursive function

--- pySpider.py
+++ pySpider.py
@@ -2,15 +2,21 @@
 #   Simple Web Crawler
 #   Assumptions
 #       * Urls to crawl are contained only in <a href="URL" >
+#       * Only absolute URLS are valid, Relative Urls are not allowed
 #
 ###
 
 import urllib2
 from bs4 import BeautifulSoup
-class PySpiderRecursive:
+from urlparse import urlparse
+class PySpider:
 
     def __init__(self):
-        self.crawled = open("crawled.txt",'w')
+        self.file_name="crawled.txt"
+        self.crawled = open(self.file_name,'w')
+        self.crawled.write("[crawled_links]\n")
+        self.repo = [] # A repository of links
+        self.limit = 0 # Limit the number of links visited. 0 means infinite
 
     def get_url(self,url):
         response = urllib2.urlopen(url)
@@ -20,19 +26,46 @@ class PySpiderRecursive:
         soup = BeautifulSoup(html)
         return soup.find_all('a')
 
+    def is_valid_url(self,url):
+        try:
+            url = urlparse(url)
+        except AttributeError as ae:
+            print ae.__str__()
+            return False
+        if url.scheme   is  ""  or  url.scheme   is None: return False
+        if url.hostname is  ""  or  url.hostname is None: return False
+        return True
+
+    def enqueue(self,url):
+        if self.is_valid_url(url): self.repo.insert(0,url)
+
+    def dequeue(self):
+        return self.repo.pop()
+
     def crawl(self,url):
-        print "crawling:" + url
-        links = self.get_all_links( self.get_url(url) )
+        if not self.is_valid_url(url): return False
+        self.enqueue(url)
+        link_count = 0
+        while True:
+            self.__crawl__()
+            link_count+=1
+            if link_count >= self.limit and self.limit > 0: break
+
+
+    def __crawl__(self):
+        url = self.dequeue()
+        print "crawling "+url
         self.crawled.write(url+"\n")
-        for link in links:
-            try:
-                self.crawl( link.get("href" ) )
-            except ValueError:
-                print "Ignoring="+link.get('href')
-                pass
+        links = self.get_all_links(self.get_url(url))
+        for link in links :
+            self.enqueue(link.get("href"))
+
     def __del__(self):
+        self.crawled.write("[un_crawled_links]\n")
+        for link in self.repo:
+            self.crawled.write(link+"\n")
         self.crawled.close()
 
 if __name__ == '__main__':
-    spider = PySpiderRecursive()
-    spider.crawl("http://gautamk.heroku.com")
+    spider = PySpider()
+    spider.crawl("http://python.org")
--- pySpiderTest.py
+++ pySpiderTest.py
@@ -13,6 +13,26 @@ class PySpiderTest(unittest.TestCase):
     def test_class(self):
         self.assertTrue(isinstance(self.spider,PySpider))
 
+    def test_url_valid(self):
+        self.assertFalse( self.spider.is_valid_url("/") )
+        self.assertFalse( self.spider.is_valid_url("http://") )
+        self.assertFalse( self.spider.is_valid_url("google.com") )
+        self.assertFalse( self.spider.is_valid_url("www.google.com") )
+        self.assertTrue(self.spider.is_valid_url("http://www.google.com") )
+    
+    def test_enqueue(self):
+        url = "http://google.com"
+        self.spider.enqueue(url)
+        self.assertEqual(url,self.spider.repo[0])
+
+    def test_dequeue(self):
+        url = "http://google.com"
+        self.spider.enqueue(url)
+        self.assertEqual(url,self.spider.dequeue())
+
+
+
+    
     def tearDown(self):
         return 
 
